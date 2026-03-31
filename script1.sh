235012fd768a7924867937dfb08e55f3678b6f3a
kanhayotube-ship-it <kanhayotube@gmail.com>
2026-03-31 19:59:12 +0530

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "===== System Info ====="
+echo "Kernel: $(uname -r)"
+echo "User: $(whoami)"
+echo "Home: $HOME"
+echo "Uptime: $(uptime)"
+echo "Date: $(date)"
+echo "License: GNU GPL"
